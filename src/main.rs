2cc65362e7fc9bd47ceba9d0bf32c2d008fd1a62
Hyeon Kim <simnalamburt@gmail.com>
2016-08-29 02:02:06 +0900

    마크다운 렌더링

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-extern crate iron;
+#[macro_use] extern crate iron;
 #[macro_use] extern crate router;
 extern crate mount;
 extern crate staticfile;
 extern crate handlebars_iron as hbs;
+extern crate pulldown_cmark as cmark;
 
 use std::collections::BTreeMap;
 use iron::prelude::*;
@@ -21,11 +22,12 @@ fn main() {
     // Routes & Mounts
     let mut chain = Chain::new(router! {
         get "/" => index,
+        get "/pages/:name" => page,
     });
-
 
     //
     // 이 뒤는 더럽습니다
+    // Reference: https://namu.wiki/w/%EC%96%B4%EC%9D%B4%20%EA%B7%B8%20%EC%95%9E%EC%9D%80%20%EC%A7%80%EC%98%A5%EC%9D%B4%EB%8B%A4
     //
 
     // Compile templates
@@ -50,12 +52,38 @@ fn main() {
     Iron::new(mount).http(addr).unwrap();
 }
 
-/// `/` Handler
+/// Root page handler
 fn index(_: &mut Request) -> IronResult<Response> {
     let mut data = BTreeMap::new();
     data.insert("content", "Hello, world!");
 
-    let mut resp = Response::new();
-    resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
-    Ok(resp)
+    Ok(Response::with((status::Ok, Template::new("default", data))))
+}
+
+/// Wiki page handler
+fn page(req: &mut Request) -> IronResult<Response> {
+    use std::fs::File;
+    use std::io::Read;
+    use router::Router;
+    use cmark::Parser;
+    use cmark::html::push_html;
+
+    // Parse URL
+    let name = req.extensions.get::<Router>().unwrap().find("name").unwrap_or("Home");
+
+    // Read file
+    let path = format!("docs/{}.md", name);
+    let mut file = itry!(File::open(&path));
+    let mut md = String::new();
+    itry!(file.read_to_string(&mut md));
+
+    // Parse markdown
+    let parser = Parser::new(&md);
+    let mut html = String::new();
+    push_html(&mut html, parser);
+
+    // Fill in to the template
+    let mut data = BTreeMap::new();
+    data.insert("content", html);
+    Ok(Response::with((status::Ok, Template::new("default", data))))
 }
