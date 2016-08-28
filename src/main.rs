99ea6a7df30f639e332a716d5338b085e584d55d
Hyeon Kim <simnalamburt@gmail.com>
2016-08-28 23:09:40 +0900

    Static file serving

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 extern crate iron;
 #[macro_use] extern crate router;
+extern crate mount;
+extern crate staticfile;
 extern crate handlebars_iron as hbs;
 
 use iron::prelude::*;
 use iron::status;
+use mount::Mount;
+use staticfile::Static;
 use hbs::{Template, HandlebarsEngine, DirectorySource};
 
 /// Entry point
 fn main() {
     //
-    // Constants
+    // Configs
     //
     let addr = "localhost:3000";
-    let templates = "./templates";
 
-    // Routes
+    // Routes & Mounts
     let mut chain = Chain::new(router! {
         get "/" => index,
     });
 
+
+    //
+    // 이 뒤는 더럽습니다
+    //
+
     // Compile templates
     let mut hbse = HandlebarsEngine::new();
-    hbse.add(Box::new(DirectorySource::new(templates, ".hbs")));
+    hbse.add(Box::new(DirectorySource::new("templates", ".hbs")));
     // load templates from all registered sources
     if let Err(r) = hbse.reload() {
         use std::error::Error;
@@ -30,9 +38,15 @@ fn main() {
     // Register handlebar middleware
     chain.link_after(hbse);
 
+    // Mount '/static/' path to the filesystem
+    let mut mount = Mount::new();
+    mount
+        .mount("/", chain)
+        .mount("/static/", Static::new("static/"));
+
     // Fire & Forget
     println!("\nServer running at \x1b[33mhttp://{}\x1b[0m\n", addr);
-    Iron::new(chain).http(addr).unwrap();
+    Iron::new(mount).http(addr).unwrap();
 }
 
 /// `/` Handler
