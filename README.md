61abf90641b58ff25d46c5b82f7f15f905b2e6e6
Hyeon Kim <simnalamburt@gmail.com>
2016-08-28 21:41:44 +0900

    Hello, world!

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Iron Practice
 
 ```bash
 cargo run
+# Go to http://localhost:3000
 ```
 
 [![](http://orig03.deviantart.net/44bc/f/2015/070/d/4/alphonse_elric_sticker_by_helixel-d8lbfvo.png)](http://fmabrotherhood.deviantart.com/art/Alphonse-Elric-Sticker-519535140)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(move |_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }).http("localhost:3000").unwrap();
 }
