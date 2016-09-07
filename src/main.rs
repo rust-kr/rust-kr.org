3b70ed434e31c4771e25ee201e6ce59155bd6452
Hyeon Kim <simnalamburt@gmail.com>
2016-09-08 01:51:00 +0900

    소스코드에도 간단한 모듈레벨 문서화 작성

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+//! [한국 러스트 사용자 그룹 홈페이지][rust-kr] 소스코드
+//! --------
+//!
+//! ### 사용법
+//! ```bash
+//! git clone https://github.com/rust-kr/rust-kr.org.wiki.git docs
+//! cargo run
+//! # 웹 브라우저에서 http://localhost:8000 를 열어보세요
+//! ```
+//!
+//! [rust-kr]: http://rust-kr.org
+
 #[macro_use(itry)] extern crate iron;
 #[macro_use(router)] extern crate router;
 extern crate mount;
