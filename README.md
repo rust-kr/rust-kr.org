99c664dc1993225a1d719b63aeb13ccf23f92ddd
Hyeon Kim <simnalamburt@gmail.com>
2016-09-11 16:36:51 +0900

    Adopt AGPLv3 license
    Closes #7

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ git clone https://github.com/rust-kr/rust-kr.org.wiki.git docs
 cargo run
 # 웹 브라우저에서 http://localhost:8000 를 열어보세요
 ```
+--------
+
+[AGPLv3](LICENSE)
 
 [rust-kr]: http://rust-kr.org
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,22 @@
 //!
 //! [rust-kr]: http://rust-kr.org
 
+// 한국 러스트 사용자 그룹 홈페이지
+// Copyright (C) 2016  Hyeon Kim
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #[macro_use(itry)] extern crate iron;
 #[macro_use(router)] extern crate router;
 extern crate mount;
